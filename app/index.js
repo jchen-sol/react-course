f7b24b4b29deeeb3e5fc9d0c26f04878087c63f3
Jason Chen <jchen@solstice.com>
2020-01-08 13:55:06 -0600

    Add ThemeContext, dark mode.

--- app/index.js
+++ app/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import Popular from './components/Popular'
 import Battle from './components/Battle'
+import { ThemeProvider } from './contexts/theme'
+import Nav from './components/Nav'
 
 function isAuthed()
 {
@@ -15,15 +17,31 @@ function isAuthed()
 //   UI
 class App extends React.Component
 {
+    constructor(props)
+    {
+        super(props)
+
+        this.state = {
+            theme: 'light',
+            toggleTheme: () => {
+                this.setState(({theme}) => ({
+                    theme: theme === 'light' ? 'dark' : 'light'
+                }))
+            }
+        }
+    }
     render()
     {
-        const authed = isAuthed();
-        const name = 'Rylan'
-
         return (
-            <div className='container'>
-                <Battle/>
-            </div>
+            <ThemeProvider value={this.state}>
+                <div className={this.state.theme}>
+                    <div className='container'>
+                        <Nav/>
+                        <Battle/>
+                    </div>
+                </div>
+            </ThemeProvider>
+            
         )
     }
 }
